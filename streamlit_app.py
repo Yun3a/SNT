26f476f51bc3207e41c8d19a644483fbabb9d8d9
Yun3a <162982989+Yun3a@users.noreply.github.com>
2024-03-18 12:06:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import nympy as np
 voc=pd.nead_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRy9rF8hUvz6P8kdm3AvK0wm1kcqkrhKhhH2apM5aLL-VTh9oHGPRdAZP0EOnCdlUNwOzCm_aJekCHm/pubhtml')
-l=voc.shape[o] 
+l=voc.shape[0] 
 i=np.random.choice(range(l))
 word_fr=voc['Définition'].values[i]
 word_chi=voc['hanzi'].values[i]
