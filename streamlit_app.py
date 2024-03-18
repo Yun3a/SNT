2839792454d0666fb2cb027c9ae9f882b567a5f7
Yun3a <162982989+Yun3a@users.noreply.github.com>
2024-03-18 11:45:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit as st
-name=st.text_input("Your name")
-st.write("Hello"+name)
+import pandas as pd
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRy9rF8hUvz6P8kdm3AvK0wm1kcqkrhKhhH2apM5aLL-VTh9oHGPRdAZP0EOnCdlUNwOzCm_aJekCHm/pubhtml')
+st.dataframe(voc)
