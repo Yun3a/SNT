8ec02617ea902dff7ad2b8013768e25e639ca1df
Yun3a <162982989+Yun3a@users.noreply.github.com>
2024-03-18 12:57:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ word_fr=voc['Définition'].values[i]
 word_chi=voc['Hanzi'].values[i]
 st.write(word_fr+"Hanzi"+word_chi)
 st.button("refresh")
-indices=np.random.choice(l,site=4,replace=False)
+indices=np.random.choice(l,size=4,replace=False)
 j=np.random.choice(indices)
 word_fr=voc["Définition"].values[j]
 st.write("Traduis:"+word_fr)
