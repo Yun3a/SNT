5e5a8bbd7084a98b0f73c34b152efd73d883e537
Yun3a <162982989+Yun3a@users.noreply.github.com>
2024-03-18 12:56:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,14 @@ word_fr=voc['Définition'].values[i]
 word_chi=voc['Hanzi'].values[i]
 st.write(word_fr+"Hanzi"+word_chi)
 st.button("refresh")
+indices=np.random.choice(l,site=4,replace=False)
+j=np.random.choice(indices)
+word_fr=voc["Définition"].values[j]
+st.write("Traduis:"+word_fr)
+for i in range(4):
+  st.button(voc["Hanzi"].values[indices[i]],on_clich=is_correct,args=[indices[i],j])
+def is_correct(i,j):
+  if i==j:
+    st.write("Bravo")
+  else:
+    st.write("Raté")
