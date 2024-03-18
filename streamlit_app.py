339ace656499eeab6a72b510eff22660d385c5d7
Yun3a <162982989+Yun3a@users.noreply.github.com>
2024-03-18 11:58:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
-voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRy9rF8hUvz6P8kdm3AvK0wm1kcqkrhKhhH2apM5aLL-VTh9oHGPRdAZP0EOnCdlUNwOzCm_aJekCHm/pubhtml')
+voc=pd.nead_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRy9rF8hUvz6P8kdm3AvK0wm1kcqkrhKhhH2apM5aLL-VTh9oHGPRdAZP0EOnCdlUNwOzCm_aJekCHm/pubhtml')
 st.dataframe(voc)
+l=voc.shape[o] 
+i=np.random.choice(range(l))
+word_fr=voc['Définition'].values[i]
+word_chi=voc['hanzi'].values[i]
+st.write(word_fr+"hanzi"+word_chi)
