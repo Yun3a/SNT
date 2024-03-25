8df72048bb1f7c6d226d03c5f0ff4f10f65ba2f6
Yun3a <162982989+Yun3a@users.noreply.github.com>
2024-03-25 12:05:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,27 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
 import numpy as np
-voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRy9rF8hUvz6P8kdm3AvK0wm1kcqkrhKhhH2apM5aLL-VTh9oHGPRdAZP0EOnCdlUNwOzCm_aJekCHm/pub?output=csv')
-l=voc.shape[0] 
-if "indices" in st.session_state:
-  indices=st.session_state["indices"]
-else:
-  indices=np.random.choice(l,size=4,replace=False)
-j=np.random.choice(indices)
-i=np.random.choice(range(l))
-word_fr=voc['Définition'].values[i]
-word_chi=voc['Hanzi'].values[i]
-st.write(word_fr+"Hanzi"+word_chi)
-st.button("refresh")
-word_fr=voc["Définition"].values[j]
-st.write("Traduis:"+" "+word_fr)
-def is_correct(i,j):
-  if i==j:
-    st.write("Bravo")
-    del st.session_state["indices"]
-  else:
-    st.write("Raté")
-    st.session_state["indices"]=indices
-for i in range(4):
-  st.button(voc["Hanzi"].values[indices[i]],on_click=is_correct,args=(indices[i],j))
+voc = pd.read_csv( https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv ')
+l = voc.shape[0]
+indices = np.random.choice(l, size=4, replace=False)
+j = np.random.choice(indices)
+word_fr=voc['Définition'].values[j]
+st.write('Traduis: '+word_fr)
 
+def is_correct(i, j):
+  if i==j:
+    st.write("Bien joué !")
+  else:
+    st.write("Perdu !")
+
+col1, col2 = st.columns(2) 
+with col1:
+    for i in range(2):
+        st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
+with col2:
+    for i in range(2,4):
+        st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
+
+
+#st.button("Seconday button")  # st.button default type is secondary
+#st.button("Primary button", type="primary")
