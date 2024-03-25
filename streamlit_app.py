95b14af237f6cee292f3748e215dcae782e2412d
Yun3a <162982989+Yun3a@users.noreply.github.com>
2024-03-25 11:55:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,11 @@ word_fr=voc["Définition"].values[j]
 st.write("Traduis:"+" "+word_fr)
 def is_correct(i,j):
   if i==j:
-    st.write("Bravo")
+    st.write("Bravo"):
+    del st.session_state["indices"]
   else:
-    st.write("Raté")
+    st.write("Raté"):
+    st.session_state["indices"]=indices
 for i in range(4):
   st.button(voc["Hanzi"].values[indices[i]],on_click=is_correct,args=(indices[i],j))
 
