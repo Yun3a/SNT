14fca1f9953619ee8d6c2a237a8be14d174a3baa
Yun3a <162982989+Yun3a@users.noreply.github.com>
2024-03-25 12:01:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,16 @@ import pandas as pd
 import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRy9rF8hUvz6P8kdm3AvK0wm1kcqkrhKhhH2apM5aLL-VTh9oHGPRdAZP0EOnCdlUNwOzCm_aJekCHm/pub?output=csv')
 l=voc.shape[0] 
-i=np.random.choice(range(l))
-word_fr=voc['Définition'].values[i]
-word_chi=voc['Hanzi'].values[i]
-st.write(word_fr+"Hanzi"+word_chi)
-st.button("refresh")
 if "indices" in st.session_state:
   indices=st.session_state["indices"]
 else:
   indices=np.random.choice(l,size=4,replace=False)
 j=np.random.choice(indices)
+i=np.random.choice(range(l))
+word_fr=voc['Définition'].values[i]
+word_chi=voc['Hanzi'].values[i]
+st.write(word_fr+"Hanzi"+word_chi)
+st.button("refresh")
 word_fr=voc["Définition"].values[j]
 st.write("Traduis:"+" "+word_fr)
 def is_correct(i,j):
